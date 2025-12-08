cd3150c30f553ed783257b9fcdfe6fdf12144480
BuildAndBreak <tiagodlapereira@gmail.com>
2025-12-08 01:42:44 +0100

    nodemailer configuration

--- Backend/utils/mailer.js
+++ Backend/utils/mailer.js
@@ -1,19 +1,28 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+export const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 export async function sendMail({ to, subject, html }) {
   try {
-    await resend.emails.send({
-      from: "Secret Santa 🎅 <onboarding@resend.dev>",
+    const info = await transporter.sendMail({
+      from: `"Secret Santa 🎅" <${process.env.SENDER_EMAIL}>`,
       to,
       subject,
       html,
     });
 
+    console.log("EMAIL SENT:", info.messageId);
     return { ok: true };
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return { ok: false, error };
+  } catch (err) {
+    console.error("EMAIL ERROR:", err);
+    return { ok: false, error: err };
   }
 }
